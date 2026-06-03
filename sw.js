75ca8f7ad88e717dd93fbb096dc545ed9391df55
Haya Balasmeh <75432575+hayabalasmeh@users.noreply.github.com>
2026-06-03 02:41:35 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open('ddi-v1').then(c=>c.addAll(['./','./index.html','./app.js'])));
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
